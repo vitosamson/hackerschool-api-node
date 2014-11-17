2b20a9ec6e40106607c07326f21630bc522264cd
Vito <vslavilla@gmail.com>
2014-11-17 14:01:51 -0500

    comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * @class hackerschool.Client
+ */
 function Client() {
   this.token = null;
 
@@ -7,6 +10,10 @@ function Client() {
   this.batches = require('./lib/batches')(this);
 }
 
+/**
+ * Sets the token for the client instance to be used in API calls
+ * @param {Object|String} token OAuth token to be used for API calls. This can be either a string with just the access token itself, or a token object returned from hackerschool.auth()
+ */
 Client.prototype.setToken = function(token) {
   this.token = token;
 };
